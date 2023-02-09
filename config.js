5b5468584736ccecdd3a66e380a246b60254b36d
jeffrey987 <jeffrey_choy@yeah.net>
2023-02-09 23:50:07 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,27 +1,13 @@
 window.config  = [
     // window.config = [
-    {
-      title: '微信路径',
-      content: [
-        {
-          name: '微信1',
-          url: '微信1'
-        },
-        {
-          labe: '微信2',
-          url: '微信2'
-        },
-        {
-          labe: '微信3',
-          url: '微信3'
-        },
-        {
-          labe: '微信4',
-          url: '微信4'
-        }
-      ]
-    }
-    
+      {
+        url: 'https://wa.me/600148725057'
+      },
+      {
+       
+        url: 'https://wa.me/600148725057'
+      },
+      
       
     
   ];
